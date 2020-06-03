9c1e1f6b4425810585346028426c0efefef428c4
Malachi Brewer <malachibre@gmail.com>
2020-06-03 18:32:48 +0000

    Page redesign

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,7 +14,17 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,10 +33,45 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+    
+  private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    Query query = new Query("Comment").addSort("year", SortDirection.DESCENDING)
+                                      .addSort("month", SortDirection.DESCENDING)
+                                      .addSort("day", SortDirection.DESCENDING);
+
+    PreparedQuery results = datastore.prepare(query);
+
+    List<Comment> comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      String text = (String) entity.getProperty("text");
+      long day = (long) entity.getProperty("day");
+      long month = (long) entity.getProperty("month");
+      long year = (long) entity.getProperty("year");
+      
+      Comment comment = new Comment(text, day, month, year);
+      comments.add(comment);
+    }
+
+    Gson gson = new Gson();
+
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(comments));
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException { 
+    Entity commentEntity = new Entity("Comment");
+
+    commentEntity.setProperty("day", Calendar.getInstance().get(Calendar.DATE));
+    commentEntity.setProperty("month", Calendar.getInstance().get(Calendar.MONTH));
+    commentEntity.setProperty("text", request.getParameter("comment"));
+    commentEntity.setProperty("year", Calendar.getInstance().get(Calendar.YEAR));
+
+    datastore.put(commentEntity);
+    
+    response.sendRedirect("/index.html");
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,14 +12,143 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
+content = {
+  "about": [
+      "About Me", 
+      "I was born on world on June 7, 2001 in Clevalnd, Ohio. My family " + 
+      "moved away from Ohio when I was around 16 months old. Since we " + 
+      "arrived in Georgia, we have also moved around several times. " +
+      "Currently we are living in a neighboorhead around 45 miles " +
+      "north of Atlanta." 
+    ], 
+    
+    "education": [
+      "Education",
+      "Although I didn't always like school growing up, " +
+      "I have also enjoyed learning new things about the world " +
+      "and other people which pushed to start appreciating my " +
+      "education more once I got to high school. The subjects I " +
+      "liked the most were always math or science. I graduated " + 
+      "high school in summer 2019 and am heading into my " +
+      "second year as a copmuter science major at the " +
+      "University of Georgia. "
+    ],
+
+    "professional": [
+      "Professional",
+      "I first started working when I was 17 years old at Banana Republic. " +
+      "I stayed for a year until I left for college. At UGA, "+
+      "I started a job working for the web team for the " +
+      "College of Agricultre and Enviornmental Science. " +
+      "I had to pause my work here to have time for my STEP internship over " +
+      "the summer, and will resume working there in August. "    
+    ],
+
+    "facts": [
+      "Random Facts",
+      "Whenever someone asks for a fun fact, my go to response is how " +
+      "I've broken both of my arms. Once when I was on a skateboard " +
+      "and once when I was on a bike. One thing that others point " +
+      "out is how I can remember the tiniest details about things. "
+    ]
+    
+}
+
+function showContent(contentName){
+  document.getElementById("content-title").innerText = 
+    content[contentName][0];
+  if (contentName === "about") {
+      loadIgIcon();
+  } else if (contentName === "professional") {
+      loadProIcons();
+  } else if (contentName === "photo-gallery") {
+      loadSelfies();
+  }
+  document.getElementById("content-text").innerText = 
+    content[contentName][1];
+}
+
+function loadIgIcon() {
+   /** instragram.svg was donloaded form flaticon.com and authored by Dave Gandy */
+   const igLink = document.createElement('a');
+   igLink.href = "https://www.instagram.com/malaxhib/";
+   const igIcon =  document.createElement('img');
+   igIcon.src = "images/instagram.svg";
+   igIcon.classList.add("icon");
+   igLink.appendChild(igIcon);
+   document.getElementById("content-title").appendChild(igLink);
+}
+
+function loadProIcons() {
+   /** linkedin.svg was donloaded form flaticon.com and authored by Dave Gandy */
+   const liLink = document.createElement('a');
+   liLink.href = "https://www.linkedin.com/in/malachi-brewer-48957b15b";
+   const liIcon =  document.createElement('img');
+   liIcon.src = "images/linkedin.svg";
+   liIcon.classList.add("icon");
+   liLink.appendChild(liIcon);
+   document.getElementById("content-title").appendChild(liLink); 
+
+   /** github.svg was donloaded form flaticon.com and authored by Dave Gandy */
+   const ghLink = document.createElement('a');
+   ghLink.href = "https://github.com/malachibre";
+   const ghIcon =  document.createElement('img');
+   ghIcon.src = "images/github.svg";
+   ghIcon.classList.add("icon");
+   ghLink.appendChild(ghIcon);
+   document.getElementById("content-title").appendChild(ghLink);
+}
+
+function loadImages() {
+  document.getElementById("content-title").innerText = "";  
+  const selfie = document.createElement("img");
+  selfie.src = "images/main-selfie.jpg";
+  selfie.classList.add("selfies");
+  document.getElementById("content-text").innerText = "";
+  document.getElementById("content-text").appendChild(selfie);
+}
+
+document.getElementById("about-me")
+  .addEventListener("click", () => showContent("about"));
+
+document.getElementById("education")
+  .addEventListener("click", () => showContent("education"));
+
+document.getElementById("professional")
+  .addEventListener("click", () => showContent("professional"));
+
+document.getElementById("random-facts")
+  .addEventListener("click", () => showContent("facts"));
+
+document.getElementById("photo-gallery")
+  .addEventListener("click", () => loadImages());
+  
+/** Retrieves data from the /data page and displays it. */
+function getComments() {
+  fetch('/data').then(response => response.json()).then((json) => {
+    json.forEach(comment => displayComment(comment));
+  });
+}
+
+/** 
+ * Creates paragraph elements and sets the text to comments 
+ * pulled from \data page. 
  */
+function displayComment(comment) {
+  const commentElement = document.createElement("p");
+  commentElement.innerText =
+   `${comment.text} posted on: ${comment.month}/${comment.day}/${comment.year}`;
+  document.getElementById("comments").appendChild(commentElement);
+}
+
+window.addEventListener('DOMContentLoaded', getComments, false);
 
 // Cycles through 3 different pictures of myself.
 let pictureNumber = 0;
 function cyclePictures() {
-  const headerSelfie = document.getElementById("header-selfie");
+  const headerSelfie = document.getElementById("gallery-picture");
   const imageNames = ["campus", "suit", "main-selfie"];
   headerSelfie.src = `images/${imageNames[pictureNumber++ % imageNames.length]}.jpg`;
 }
+
+
